b49f3c09ac92d3cc3830d82c86758b063a1cf197
Nely Nehemia <nelyne1014@gmail.com>
2023-07-30 14:49:10 +0300

    fix MaxCfyLint

--- server/src/cloudify/cloudify.ts
+++ server/src/cloudify/cloudify.ts
@@ -10,7 +10,7 @@ import {TextDocument} from 'vscode-languageserver-textdocument';
 import {nodeTemplates} from './constants/default-node-template-properties';
 import {CompletionItem, Diagnostic, TextDocumentPositionParams} from 'vscode-languageserver/node';
 import {readFile, documentCursor} from './parsing';
-import { cfyLint, maxCfyLint } from './cfy-lint';
+import { cfyLint } from './cfy-lint';
 import { words } from './word-completion';
 import {
     isPair,
@@ -55,6 +55,9 @@ import {
     keywords as nodeTemplateKeywords,
 } from './sections/node-templates';
 
+
+const MAX_CFY_LINT_PROCESSES = 3;
+let ConcurrentProcesses = 0;
 
 export class CloudifyWords extends words {
 
@@ -204,11 +207,14 @@ export class CloudifyWords extends words {
             }
         }
         
-        if (this.cfyLintTimer.isReady() && maxCfyLint()) {
-            this.diagnostics = await cfyLint(textDocument).then((result) => {return result;});
-            this.diagnostics = [];
+        if (this.cfyLintTimer.isReady()) {
+            if (ConcurrentProcesses < MAX_CFY_LINT_PROCESSES){
+                ConcurrentProcesses += 1;
+                this.diagnostics = await cfyLint(textDocument).then((result) => {return result;});
+                this.diagnostics = [];
+                ConcurrentProcesses -= 1;
+            }
         }
-
         await this.privateRefresh();
     }
 
